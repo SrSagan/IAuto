0368298c0887116c4a038b0e6b081b3b4cc18669
SrSagan <santiago.daldex@gmail.com>
2021-08-05 12:32:39 -0300

    control

--- control.py
+++ control.py
@@ -1,32 +1,25 @@
 import serial
-import keyboard
 from pynput import keyboard
 
 s = serial.Serial(port='/dev/ttyUSB0', baudrate=115200, bytesize=8)
 
-'''while True:
-    if keyboard.is_pressed('w'):
-        print("test")
-        s.write(b"1")
-    if keyboard.is_pressed('a'):
-        print("test")
-        s.write(b"4")
-    if keyboard.is_pressed('d'):
-        print("test")
-        s.write(b"3")
-    if keyboard.is_pressed('s'):
-        print("test")
-        s.write(b"2")'''
-
 def on_key_press(key):
-    print("\npressed key %s" % key)
-    input_value = "90"
+    print(str(key))
+    if key.char == ('w'):
+        input_value = "90!90"
+    elif key.char == ('s'):
+        input_value = "-90!-90"
+    elif key.char == ('a'):
+        input_value = "-90!90"
+    elif key.char == ('d'):
+        input_value = "90!-90"
+    print(input_value)
     s.write(input_value.encode())
     return False
 
 def on_key_release(key):
     print("\nreleased key %s" % key)
-    input_value = "-1"
+    input_value = "0!0"
     s.write(input_value.encode())
     return False
 
