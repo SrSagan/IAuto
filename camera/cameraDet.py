3f956dc43da71102c9f89307b1888179a15450cc
SrSagan <santiago.daldex@gmail.com>
2021-08-26 17:08:10 -0300

    camera

--- camera/cameraDet.py
+++ camera/cameraDet.py
@@ -1,9 +1,12 @@
+import argparse
+import imutils
 import cv2
+
 
 class ShapeDetector:
     def __init__(self):
         pass
-    
+
     def detect(self, c):
         shape = 'unidentified'
         peri = cv2.arcLenght(c, True)
@@ -13,13 +16,11 @@ class ShapeDetector:
             (x, y, w, h) = cv2.boundingRect(approx)
             ar = w / float(h)
             shape = "square" if ar >= 0.95 and ar <= 1.05 else "rectangle"
-        else: shape = "Desconocido"
+        else:
+            shape = "Desconocido"
 
         return shape
 
-import argparse
-import imutils
-import cv2
 
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True, help="path to img")
@@ -29,16 +30,27 @@ image = cv2.imread(args["image"])
 resized = imutils.resize(image, width=300)
 ratio = image.shape[0] / float(resized.shape[0])
 
-gray= cv2.cvtColor(resized, cv2.COLOR_BGT2GRAY)
-blurred = cv2.GaussianBlur(gray, (5,5), 0)
+gray = cv2.cvtColor(resized, cv2.COLOR_BGT2GRAY)
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 thresh = cv2.threshold(blurred, 60, 255, cv2.THRESH_BINARY)[1]
 
-cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                        cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
 sd = ShapeDetector()
 
 for c in cnts:
     M = cv2.moments(c)
-    cX = int((M["m10"] /M["m00"]) * ratio)
-    cY = int((M["m01"] /M["m00"]) * ratio)
+    cX = int((M["m10"] / M["m00"]) * ratio)
+    cY = int((M["m01"] / M["m00"]) * ratio)
     shape = sd.detect(c)
+
+    c = c.astype("float")
+    c *= ratio
+    c = c.astype("int")
+    cv2.drawContours(image, [c], -1, (0, 255, 0), 2)
+    cv2.putText(image, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
+                0.5, (255, 255, 255), 2)
+    # show the output image
+    cv2.imshow("Image", image)
+    cv2.waitKey(0)
