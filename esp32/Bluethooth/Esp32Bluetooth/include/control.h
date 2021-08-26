b899f4506410538df86e0062060dd58d59e78283
SrSagan <santiago.daldex@gmail.com>
2021-08-26 16:13:23 -0300

    libreria

---
+++ esp32/Bluethooth/Esp32Bluetooth/include/control.h
@@ -0,0 +1,14 @@
+#include <Arduino.h>
+
+//---------PINES---------//
+#define INT1 27
+#define INT2 5
+#define INT3 18
+#define INT4 19
+#define INT1_CHANNEL 0
+#define INT2_CHANNEL 1
+#define INT3_CHANNEL 2
+#define INT4_CHANNEL 3
+#define RESOLUTION 10
+#define FREQUENCY 30000
+
